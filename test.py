9e1e65cf0b5b76b10467194bf5bd690cd7ab477d
Hasan <MozumderHasan>
2020-12-04 23:50:47 +0600

    changes in master branch for loop

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 print("New practice of git")
-a=input("")
-a=int(a)
-if a<21:
+a = input("")
+a = int(a)
+if a < 21:
     print("He is not eligible for marrige")
-elif 21<a:
+elif 21 < a:
     print("He is eligible for marrige")
 
 for b in range(10):
